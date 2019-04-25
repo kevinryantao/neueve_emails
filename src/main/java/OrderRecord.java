9b41627ac12f41af2125ebf5aa225f12f81ac89c
Kevin Tao <kevin.ryan.tao@gmail.com>
2019-04-24 22:38:37 -0500

    PayPal Daily CSV Parser

---
+++ src/main/java/OrderRecord.java
@@ -0,0 +1,95 @@
+import org.apache.commons.lang3.StringUtils;
+import org.joda.time.DateTime;
+
+import java.text.DecimalFormat;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class OrderRecord {
+
+    private static DecimalFormat HUNDREDTHS = new DecimalFormat("#.00");
+
+    String name;
+    String firstName;
+    String lastName;
+
+    String email;
+    String address1;
+    String address2;
+    String city;
+    String state;
+    String zipcode;
+    String country;
+    Map<String, Integer> orderQuantities = new HashMap<String, Integer>();
+
+
+    int numPurchases;
+    DateTime firstPurchase = null;
+    DateTime lastPurchase = null;
+    String address;
+    double lifetimeValue;
+    List<String> orderHistory;
+    int numSubscriptions;
+    int numBVPurchases;
+
+    public OrderRecord(String name, String email, String address){
+        this.name = name;
+        this.email = email;
+
+        this.address = address;
+
+        String[] addressParts = this.address.split(",");
+        firstName = addressParts[0].toLowerCase().replaceAll("[^a-zA-Z ]", " ").trim();
+        lastName = addressParts[1].trim();
+        if(lastName.matches(".*\\d+.*") || lastName.toLowerCase().contains("box ")){
+            String[] firstNameSplit = firstName.split(" ");
+            if(firstNameSplit.length > 1){
+                lastName = firstNameSplit[firstNameSplit.length -1];
+                firstName = firstName.substring(0, firstName.length() - lastName.length()).trim();
+            }
+        }
+        String[] firstNameParts = firstName.split(" ");
+        firstName = "";
+        for(int i = 0; i < firstNameParts.length; i++){
+            if(firstNameParts[i].length() > 1){
+                firstNameParts[i] = firstNameParts[i].substring(0,1).toUpperCase() + firstNameParts[i].substring(1);
+            } else {
+                firstNameParts[i] = firstNameParts[i].toUpperCase();
+            }
+        }
+        firstName = StringUtils.join(firstNameParts, " ");
+
+    }
+
+    public void addPurchase(double gross, String type, DateTime purchaseDate, String itemTitle){
+        if(type.equals("Subscription Payment")){
+            numSubscriptions++;
+            lifetimeValue += gross;
+        } else if(type.equals("Website Payment") || type.equals("eBay Auction Payment")){
+            numPurchases++;
+            lifetimeValue += gross;
+        } else if(type.equals("Shopping Cart Item")){
+            if(itemTitle.contains("bv") || itemTitle.contains("BV")){
+                numBVPurchases++;
+            }
+        }
+
+        if(firstPurchase == null || firstPurchase.isAfter(purchaseDate)){
+            firstPurchase = purchaseDate;
+        }
+
+        if(lastPurchase == null || lastPurchase.isBefore(purchaseDate)){
+            lastPurchase = purchaseDate;
+        }
+    }
+
+    public String toString(){
+
+        String[] array = new String[] { email, "\"" + name + "\"", "\"" + firstName + "\"", "\"" + lastName + "\"", "\"" + address + "\"", HUNDREDTHS.format(lifetimeValue), Integer.toString(numPurchases), Integer.toString(numSubscriptions), Integer.toString(numBVPurchases), firstPurchase.toLocalDate().toString(), lastPurchase.toLocalDate().toString() };
+
+        return StringUtils.join(array, ",");
+
+    }
+
+}
---
+++ src/main/java/PayPalDailyCSVParser.java
@@ -0,0 +1,62 @@
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVParser;
+import org.apache.commons.csv.CSVRecord;
+import org.joda.time.DateTime;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.HashMap;
+
+import static java.nio.charset.StandardCharsets.US_ASCII;
+
+// test comment
+
+public class PayPalDailyCSVParser {
+
+
+    public static void main(String[] args) throws IOException {
+
+        File source = new File("Download05-03-2018.CSV");
+
+        CSVParser parser = CSVParser.parse(source, US_ASCII, CSVFormat.EXCEL.withHeader());
+
+        System.out.println(parser.getHeaderMap());
+        HashMap<String, CustomerRecord> emailToCustomerRecordMap = new HashMap<String, CustomerRecord>();
+
+        PrintWriter writer = new PrintWriter("CustomerDataCleanJan.csv", "UTF-8");
+
+
+        for (CSVRecord csvRecord : parser) {
+            String from = csvRecord.get("From Email Address").toLowerCase();
+            if (from.equals("neueve.suppositories@gmail.com")) {
+                continue;
+            }
+            CustomerRecord customerRecord = emailToCustomerRecordMap.get(from);
+            if (customerRecord == null) {
+                customerRecord = new CustomerRecord(csvRecord.get("Name"), from, csvRecord.get("Shipping Address"));
+                emailToCustomerRecordMap.put(from, customerRecord);
+            }
+            String[] monthDateYear = csvRecord.get(0).split("/");
+            customerRecord.addPurchase(
+                    Double.parseDouble(csvRecord.get("Gross")),
+                    csvRecord.get("Type"),
+                    new DateTime()
+                            .withYear(Integer.parseInt(monthDateYear[2]))
+                            .withMonthOfYear(Integer.parseInt(monthDateYear[0]))
+                            .withDayOfMonth(Integer.parseInt(monthDateYear[1])),
+                    csvRecord.get("Item Title"));
+        }
+
+        System.out.println(emailToCustomerRecordMap.size());
+
+        writer.println("Email,Name,First Name,Last Name,Address,Lifetime Value,Num Purchases,Num Subs,Num BV,First Purchase,Last Purchase");
+
+        for(CustomerRecord customerRecord : emailToCustomerRecordMap.values()){
+            writer.println(customerRecord);
+        }
+
+        writer.close();
+
+    }
+}
